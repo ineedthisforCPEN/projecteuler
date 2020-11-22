6e03becf58fc158431a9342f8c13c63c6eb6670d
ineedthisforCPEN <mih.stefanovic@gmail.com>
2020-11-22 01:52:57 -0800

    Add utilities for problem resources
    Some problems come with values that come from text files (i.e.
    resources). This provides utilities to read these resources.

---
+++ utils/resources.py
@@ -0,0 +1,47 @@
+import collections
+import os.path
+
+from projecteuler import constants as const
+
+
+_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+_RESOURCE_DIR = os.path.abspath(os.path.join(_BASE_DIR,
+                                             "..",
+                                             "resources"))
+
+_STRING_TO_TYPE_MAP = collections.defaultdict(lambda: _nonetypeclass, {
+    "integer": int,
+    "float": float,
+    "string": str,
+})
+
+
+def _nonetypeclass(*args, **kwargs):
+    """Initializer function that returns Nonetype no matter what."""
+    return None
+
+
+def load_problem_resources(problem):
+    """Load the resources for a specific problem.
+
+    Parameters:
+        problem     The problem number whose resources to load
+
+    Return:
+        Return problem's resources. Returns None if no resources were
+        found or if the resources were not loaded as expected.
+    """
+    filename = os.path.join(_RESOURCE_DIR,
+                            const.RESOURCE_FILE.format(problem))
+    if not os.path.exists(filename):
+        return None
+
+    value = None
+    with open(filename, "r") as openfile:
+        lines = openfile.read().splitlines()
+        typeclass = _STRING_TO_TYPE_MAP[lines[0]]
+
+        raw = "".join(lines[1:])
+        value = typeclass(raw)
+
+    return value
